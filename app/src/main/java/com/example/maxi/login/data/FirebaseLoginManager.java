6b982412bdddb07b2548e72550febfdf5a9459e1
mruiznie <maximiliano.ruiz@intel.com>
2016-07-12 16:38:33 -0300

    create new user

--- app/src/main/java/com/example/maxi/login/data/FirebaseLoginManager.java
+++ app/src/main/java/com/example/maxi/login/data/FirebaseLoginManager.java
@@ -8,20 +8,28 @@ import android.util.Log;
 
 import com.example.maxi.login.R;
 import com.example.maxi.login.activity.MainActivity;
+import com.example.maxi.login.entity.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FirebaseLoginManager implements FirebaseLoginInterface {
 
     public static String TAG = "FirebaseLoginManager";
 
     protected FirebaseAuth auth;
+    private FirebaseDatabase database;
     protected FirebaseAuth.AuthStateListener mAuthListener;
 
     private ProgressDialog progressDialog;
 
     public FirebaseLoginManager() {
         auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
     }
 
     public FirebaseAuth getAuth() {
@@ -70,7 +78,32 @@ public class FirebaseLoginManager implements FirebaseLoginInterface {
     }
 
     protected void dismissDialog() {
-        progressDialog.dismiss();
+       // progressDialog.dismiss();
     }
 
+    protected void registerUser() {
+        final String uid = auth.getCurrentUser().getUid();
+        final DatabaseReference myRef = database.getReference("users/" + uid);
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                if (user == null) {
+                    user = new User();
+                    user.setMail(auth.getCurrentUser().getEmail());
+                    myRef.setValue(user);
+                } else {
+                    Log.d(TAG, "Value is: " + user);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+    }
 }
